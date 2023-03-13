cd9dbd261214c578fc403947dc31531590ac0297
huanghuiqiang <huanghuiqiang86@gmail.com>
2023-03-13 22:03:11 +0800

    feat: add page 5

--- test-vite/counter.js
+++ test-vite/counter.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import lodashES from 'lodash-es';
 console.log("lodash", _);
 
 export const count = 0;
---
+++ test-vite/vite.config.js
@@ -0,0 +1,5 @@
+export default {
+    // optimizeDeps: {
+    //     exclude: ["lodash-es"] // 当遇到lodash-es的时候，不进行依赖预构建
+    // }
+}
